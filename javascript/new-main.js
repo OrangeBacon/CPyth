a5969b3e86c10851fd718d47ffb9fd2681e83aca
ScratchOs <Computer.backup.15@gmail.com>
2016-05-02 19:20:13 +0100

    Windows v2.0

--- javascript/new-main.js
+++ javascript/new-main.js
@@ -1,6 +1,8 @@
 var cpyth = {
   vars: {
-    codemirror:""
+    codemirror:"",
+	windowTemplate: '<div class="window" id="<id>"><div class="window-header"><div class="window-header-close" data-id="<id>" id="window-header-close-<id>"><p class="window-header-close-text" onclick="ui.closeWindow(<id>)">X</p></div><div class="window-header-dock"><p class="window-header-dock-text">X</p></div><p class="window-header-title"><title></p></div><content></div>',
+	windows: []
   },
   utils: {
     ajax(method="GET",url="/",cont=function(){},head="") {
@@ -8,31 +10,89 @@ var cpyth = {
       xhttp.onreadystatechange = function(){cont(xhttp.responseText)};
       xhttp.open(method, url, true);
       xhttp.send();
+    },
+	dragMoveListener (event) {
+      var target = event.target,
+	    x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
+        y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
+      target.style.webkitTransform =
+      target.style.transform =
+        'translate(' + x + 'px, ' + y + 'px)';
+      target.setAttribute('data-x', x);
+      target.setAttribute('data-y', y);
+    },
+	resizeListener(event) {
+	  if(event.target.getAttribute('data-resize') != 'false'){
+        var target = event.target,
+          x = (parseFloat(target.getAttribute('data-x')) || 0),
+          y = (parseFloat(target.getAttribute('data-y')) || 0);
+        target.style.width  = Math.min(Math.max(parseInt(event.rect.width), parseInt(target.getAttribute('data-minX')||event.rect.width)), parseInt(target.getAttribute('data-maxX')||0)) + 'px';
+        target.style.height = Math.min(Math.max(parseInt(event.rect.height), parseInt(target.getAttribute('data-minY')||event.rect.height)), parseInt(target.getAttribute('data-maxY')||0))+ 'px';
+        x += event.deltaRect.left;
+        y += event.deltaRect.top;
+        target.style.webkitTransform = 
+	    target.style.transform =
+          'translate(' + x + 'px,' + y + 'px)';
+        target.setAttribute('data-x', x);
+        target.setAttribute('data-y', y);
+	  };
+    }
+  },
+  window: {
+    window(title,content,options) {
+	  var id = (cpyth.vars.windows[cpyth.vars.windows.length-1]||0) + 1;
+      var Window = cpyth.vars.windowTemplate;
+	  Window = Window.replace(/<id>/g,id).replace(/<title>/g,title).replace(/<content>/g,content);
+	  document.getElementById("content").innerHTML = document.getElementById("content").innerHTML + Window;
+	  Window = document.getElementById(id);
+	  if(options != undefined){
+  	    if(options.keepAspect){
+	      Window.setAttribute("class","window-aspect");
+  	    };
+	    if(options.resize){
+	      Window.setAttribute("data-resize",options.resize||false);
+  	    };
+		Window.setAttribute("data-minX",options.minX||0);
+		Window.setAttribute("data-minY",options.minY||0);
+		Window.setAttribute("data-maxX",options.maxX||Infinity);
+		Window.setAttribute("data-maxY",options.maxY||Infinity);
+		Window.style.width = options.startX + "px";
+		Window.style.height = options.startY + "px";
+	  };
+	  cpyth.vars.windows.push(id);
+	  return id;
+    },
+	closeWindow(id){
+      document.getElementById("content").removeChild(document.getElementById(id));
+	  var index = cpyth.vars.windows.indexOf(id);
+	  if (index > -1) {
+        cpyth.vars.windows.splice(index, 1);
+      };
     }
   },
   console: {
-    codeError(message){
+    error(message){
       var node = document.createElement("p");
       var textnode = document.createTextNode(message);
       node.appendChild(textnode);
 	  node.setAttribute("class","error");
       document.getElementById("content-console-output-main").appendChild(node);
     },
-    codeWarning(message){
+    warning(message){
       var node = document.createElement("p");
       var textnode = document.createTextNode(message);
       node.appendChild(textnode);
 	  node.setAttribute("class","warning");
       document.getElementById("content-console-output-main").appendChild(node);
     },
-    codeInfo(message){
+    info(message){
       var node = document.createElement("p");
       var textnode = document.createTextNode(message);
       node.appendChild(textnode);
 	  node.setAttribute("class","info");
       document.getElementById("content-console-output-main").appendChild(node);
     },
-    codeText(message){
+    text(message){
       var node = document.createElement("p");
       var textnode = document.createTextNode(message);
       node.appendChild(textnode);
@@ -43,7 +103,27 @@ var cpyth = {
   init() {
     var host = document.getElementById("content-codemirror-container");
     cpyth.vars.codemirror = CodeMirror(host,{lineNumbers: true,viewportMargin:Infinity,fixedGutter:true});
-	cpyth.console.codeText("Welcome to the CPyth online interpreter.");
+	cpyth.console.text("Welcome to the CPyth online interpreter.");
+	interact(".window").draggable({
+      restrict: {
+        restriction: '#content',
+  	    elementRect: { top: 0, left: 0, bottom: 1, right: 1 }
+      },
+	  onmove: cpyth.utils.dragMoveListener
+    }).resizable({
+      edges: { left: false, right: true, bottom: true, top: false }
+    }).on('resizemove',cpyth.utils.resizeListener);
+	interact(".window-aspect").draggable({
+      restrict: {
+        restriction: '#content',
+  	    elementRect: { top: 0, left: 0, bottom: 1, right: 1 }
+      },
+	  onmove: cpyth.utils.dragMoveListener
+    }).resizable({
+	  preserveAspectRatio:true,
+      edges: { left: false, right: true, bottom: true, top: false }
+    }).on('resizemove',cpyth.utils.resizeListener);
+	cpyth.window.window("hello","Welcome to CPyth",{resize:true,keepAspect:true,maxX:300,maxY:300,minX:100,minY:100,startX:200,startY:200});
   }
 };
 window.onload = function(){
